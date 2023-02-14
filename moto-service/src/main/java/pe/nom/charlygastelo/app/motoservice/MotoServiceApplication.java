6fd0ff5981cbd3bbff77541c31c9b4303060c283
Charly Gastelo <charly.gastelo@memorandum.net>
2023-02-14 07:09:04 -0500

    eureka

--- moto-service/src/main/java/pe/nom/charlygastelo/app/motoservice/MotoServiceApplication.java
+++ moto-service/src/main/java/pe/nom/charlygastelo/app/motoservice/MotoServiceApplication.java
@@ -2,8 +2,10 @@ package pe.nom.charlygastelo.app.motoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MotoServiceApplication {
 
     public static void main(String[] args) {
