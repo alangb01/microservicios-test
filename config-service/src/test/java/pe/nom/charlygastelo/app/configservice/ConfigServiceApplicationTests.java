c28645d0bac84a5424f90262862bf895a224141b
Charly Gastelo <charly.gastelo@memorandum.net>
2023-02-13 20:15:53 -0500

    update

---
+++ config-service/src/test/java/pe/nom/charlygastelo/app/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.nom.charlygastelo.app.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
